2ccc8f46eab03906bb96b717fb1a64f05f76fc30
Gursimrat Rai <gursimratsinghrai@gmail.com>
2021-08-19 20:59:22 +0530

    make card editable

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,24 +1,27 @@
 import React from "react";
 
-import { Card, Avatar,Button , Rate } from "antd";
+import { Card, Avatar,Button , Rate ,Input , InputNumber} from "antd";
 
 const { Meta } = Card;
 
 const Product = (props) => {
-  const { product } = props;
+  const { product , edit , handleEdit,handleCancel } = props;
   return (
     <div>
-      <Card hoverable style={{ width: '80vw',marginTop: 10 }}  actions={[
-            <i className="fas fa-pencil-alt"></i>,
+      <Card hoverable style={{ width: '80vw',marginTop: 10 }} actions={edit===product.id?[
+            <Button onClick={handleCancel} type='secondary'>Cancel</Button>,        
+            <Button type='primary'>Save</Button>,        
+          ]:[
+            <i onClick={()=> handleEdit(product.id)} className="fas fa-pencil-alt"></i>,
             <i className="fas fa-trash"></i>,
             <Button type='primary'>Add To Cart</Button>,        
           ]}>
         <Meta 
-          avatar={<Avatar src={product.image} style={{width:100 , height:100}} />}
-          title={product.title}
-          description={`Rs ${product.price}`}
+          avatar = {<Avatar src={product.image} style={{width:100 , height:100}} />}
+          title = {edit===product.id?<Input defaultValue={product.title} /> : product.title}
+          description = {edit===product.id?<span>Rs <InputNumber min={0} defaultValue={product.price} /></span>: <span>Rs {product.price}</span>}
         />
-        <Rate allowHalf defaultValue={product.rating} />
+        { edit===product.id?<Rate defaultValue={product.rating}/>:<Rate disabled defaultValue={product.rating}/> }
       </Card>
     </div>
   );
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React ,{ useState } from 'react';
 import { Product } from './';
 
 function ProductList(props) {
     const {products} = props;
+    const [edit,setEdit] = useState(-1);
+
+    const handleEdit = (id) => {
+        setEdit(id);
+    }
+    const handleCancel = () => {
+        setEdit(-1);
+    }
+
     console.log('products',products);
     return (
         <div>
             {products.map((product) => {
-                return <Product key={product.id} product={product} />
+                return <Product edit = {edit} handleEdit={handleEdit} handleCancel={handleCancel} key={product.id} product={product} />
             })}
          </div>
     );
